78f0ba7a311cc7e1a00fb5feb9176d80f7aa3e62
Cheng-Ling Lai <jamesljlster@gmail.com>
2025-10-16 15:36:01 +0800

    Updated codecompanion config

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -5,23 +5,29 @@ return {
     "olimorris/codecompanion.nvim",
     opts = {
       adapters = {
-        openai_compatible = function()
-          return require("codecompanion.adapters").extend("openai_compatible", {
-            env = {
-              api_key = os.getenv "CODECOMPANION_OPENAI_API_KEY",
-              url = "https://llm.ai.himax.com.tw",
-            },
-          })
-        end,
+        http = {
+          openai_compatible = function()
+            return require("codecompanion.adapters").extend("openai_compatible", {
+              env = {
+                api_key = os.getenv "CODECOMPANION_OPENAI_API_KEY",
+                url = "https://llm.ai.himax.com.tw",
+              },
+            })
+          end,
+        },
       },
       strategies = {
         chat = {
-          adapter = "openai_compatible",
-          model = "meta-llama/Llama-3.3-70B-Instruct",
+          adapter = {
+            name = "openai_compatible",
+            model = "openai/gpt-oss-120b",
+          },
         },
         inline = {
-          adapter = "openai_compatible",
-          model = "meta-llama/Llama-3.3-70B-Instruct",
+          adapter = {
+            name = "openai_compatible",
+            model = "openai/gpt-oss-120b",
+          },
         },
       },
     },
